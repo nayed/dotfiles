8399b08dc7310f425ae5713702a314752a2669e2
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-05-11 01:11:36 +0200

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,9 +8,6 @@ end
 # add git autocomplete to hub
 complete -c hub -w git
 
-# jump
-status --is-interactive; and . (jump shell | psub)
-
 # cd and ls
 function chpwd --on-variable PWD
   status --is-command-substitution; and return
@@ -29,9 +26,12 @@ set -g FZF_CTRL_T_OPTS "--preview 'coderay {} 2> /dev/null | head -200'"
 set -g FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
 set -g FZF_DEFAULT_OPTS '--height 70% --reverse'
 
-# enable rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
 set -x -U GOPATH $HOME/Codes/go
 set -x -U GOBIN $GOPATH/bin
 set -gx PATH $PATH $GOBIN
+
+# enable rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
+# jump
+status --is-interactive; and source (jump shell | psub)
